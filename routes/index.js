572dafe4b9dc982acd6184da37b5adcd49dabc91
agung <agung.visione@gmail.com>
2023-03-06 11:56:21 +0700

    fix: model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const user = require('./../controllers/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/users/all",user.getAll);
 
 module.exports = router;
