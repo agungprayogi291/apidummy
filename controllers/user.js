f566c8df1d88d72142be91033b0d452fe2e40757
agung <agung.visione@gmail.com>
2023-03-07 09:51:50 +0700

    feat: add user

--- controllers/user.js
+++ controllers/user.js
@@ -12,10 +12,29 @@ user.getAll = async(req,res) =>{
         });
     }catch(error){
         res.status(500).json({
-            status:"error",
+            status:"error", 
             data:error
         });
     }
 };
+user.create = async (req,res) =>{
+    const {firstName,lastName,email} = req.body;
+    try{
+        let result = await User.create({
+            "firstName" : firstName,
+            "lastName" : lastName,
+            "email" : email
+        });
+        return res.json({
+            status:"success",
+            data: result
+        });
+    }catch(error){
+        res.status(500).json({
+            status:"error",
+            data:error
+        })
+    }
+}
 
 module.exports = user;
